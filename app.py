926b331e0a3af8cdfa6a74f348c558f74526da0a
Truc Tran <tracytrle@gmail.com>
2024-07-10 23:16:45 -0500

    created register and login

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
-from flask import Flask
+# app.py
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
 
 app = Flask(__name__)
 
+# Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tracytrle:truc123@localhost:5432/medic_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = '87e5956fcac3450689225a8c8facade2'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 @app.route('/')
-def home():
-    return 'Hello, Flask!'
+def index():
+    return "Hello MedicAI"
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message="User created successfully"), 201
+    # return jsonify({
+    #     "username": new_user.username,
+    #     "password": new_user.password
+    # })
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity={'username': user.username})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message="Invalid credentials"), 401
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 if __name__ == '__main__':
+    with app.app_context():
+      db.create_all()
+
     app.run(debug=True)
