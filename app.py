c0614d7f41e968ad0e3ff8cbce07ec24b978c659
Truong Le <truong.le@nuvoair.com>
2024-07-20 14:06:43 -0500

    add missing deps

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-# app.py
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import os
 from flask_cors import CORS
 from models.user import db, User
 from config import ApplicationConfig
@@ -18,10 +15,6 @@ jwt = JWTManager(app)
 db.init_app(app)
 
 
-@app.route('/')
-def index():
-    return "Hello MedicAI"
-
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -33,11 +26,11 @@ def register():
     new_user = User(email=data['email'], password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    # return jsonify(message="User created successfully"), 201
     return jsonify({
         "email": new_user.email,
         "password": data['password']
     })
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -45,21 +38,21 @@ def login():
     user = User.query.filter_by(email=data['email']).first()
     if user and bcrypt.check_password_hash(user.password, data['password']):
         access_token = create_access_token(identity={'email': user.email})
-        # return jsonify(access_token=access_token), 200
         return jsonify({
-        "id": user.id,
-        "email": user.email,
-        "access_token": access_token
-    }), 200
+            "id": user.id,
+            "email": user.email,
+            "access_token": access_token
+        }), 200
     else:
         return jsonify(message="Invalid credentials"), 401
+
 
 @app.route('/protected', methods=['GET'])
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
-    # print("current user: " + current_user)
     return jsonify(logged_in_as=current_user), 200
+
 
 if __name__ == '__main__':
     with app.app_context():
--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
-from pathlib import Path
-
 
 load_dotenv()
 
+
 class ApplicationConfig:
-  JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
-  SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
   
     
