a1048757096080f1050498e600710d3ac2709fe7
Truc Tran <tracytrle@gmail.com>
2024-07-20 16:48:31 -0500

    resolved conflicts when merging from main

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_migrate import Migrate
 from models.user import db, User
 from config.config import ApplicationConfig
