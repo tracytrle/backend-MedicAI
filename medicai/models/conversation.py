55013dd72fc265a832e5214b632fa851fc367744
Truc Tran <tracytrle@gmail.com>
2024-08-21 23:22:42 -0500

    create conversation table

---
+++ medicai/models/conversation.py
@@ -0,0 +1,16 @@
+
+
+from medicai.extensions import db
+from datetime import datetime
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    conversationId = db.Column(db.Integer, nullable=False)
+    sender = db.Column(db.String(50), nullable=True)
+    message = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    last_updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"Conversation('{self.userId}', '{self.message}', '{self.response}')"
